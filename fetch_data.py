f5935ec58f1c7d750a38bac93dcdd55f65625125
Chintan Sawla <csawla@purdue.edu>
2024-12-19 18:19:55 -0800

    clean up files

--- fetch_data.py
+++ fetch_data.py
@@ -1,34 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
 from zipfile import ZipFile
-import os
 import pandas as pd
-import json
 
 url = f"https://cricsheet.org/downloads"
 
 LEAGUES = ["ipl", "bbl", "sma", "sat",
-           "mlc", "cpl", "psl", "ntb", "lpl", "bpl"]
-
-
-# for league in LEAGUES:
-#     format_url = f"{url}/{league}_json.zip"
-#     r = requests.get(format_url)
-#     if r.status_code == 200:
-#         with open(f"./{league}.zip", 'wb') as zip:
-#             zip.write(r.content)
-
-#         with ZipFile(f"{league}.zip", "r") as zip:
-#             filenames = zip.namelist()
-#             # txt = [x for x in filenames if x.endswith(".txt")]
-#             for file in filenames:
-#                 with zip.open(file) as zip_file:
-#                     content = zip_file.read().decode()
-#                 if file.endswith(".txt"):
-#                     zip.extract(file, f"./{league}.txt")
-#                 print(content)
-#                 break
-#     break
+           "mlc", "cpl", "psl", "ntb", "lpl", "bpl", "t20s"]
 
 for league in LEAGUES:
     format_url = f"{url}/{league}_json.zip"
@@ -59,6 +36,3 @@ for league in LEAGUES:
             meta_df['date'] = pd.to_datetime(meta_df['date'])
             meta_df = meta_df.sort_values("date", ignore_index=True)
             meta_df.to_csv(f'./data/util/{league}.csv', index=False)
-
-
-
